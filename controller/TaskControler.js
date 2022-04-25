34b3c534fb666485d5f90a903d9271f83efb08ed
ricardoalexandrepereira <ricardo_certe@hotmail.com>
2022-04-25 15:31:18 +0100

    a fucionar perfeito

--- controller/TaskControler.js
+++ controller/TaskControler.js
@@ -3,27 +3,66 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const taskList = await Task.find();
-    return res.render("index", {taskList});
+    return res.render("index", { taskList, task: null, taskDelete: null });
   } catch (err) {
-    res.status(500), send({ error: err.message });
+    res.status(500).send({ error: err.message });
   }
 };
 
 const createTask = async (req, res) => {
   const task = req.body;
 
-  if (!task) {
+  if (!task.task) {
     return res.redirect("/");
   }
+
   try {
     await Task.create(task);
     return res.redirect("/");
   } catch (err) {
-    res.status(500), send({ error: err.message });
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const taskList = await Task.find();
+
+    if (req.params.method == "update") {
+      const task = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task, taskDelete: null, taskList });
+    } else {
+      const taskDelete = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task: null, taskDelete, taskList });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const updateOneTask = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const deleteOneTask = async (req, res) => {
+  try {
+    await Task.deleteOne({ _id: req.params.id });
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
   }
 };
 
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
+  updateOneTask,
+  deleteOneTask,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,8 @@ const TaskControler = require("../controller/TaskControler");
 
 routes.get("/", TaskControler.getAllTasks);
 routes.post("/create", TaskControler.createTask);
+routes.get("/getById/:id/:method", TaskControler.getById);
+routes.post("/updateOne/:id", TaskControler.updateOneTask);
+routes.get("/deleteOne/:id", TaskControler.deleteOneTask);
 
 module.exports = routes;
