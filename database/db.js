e117b1b13013fd0a19a8b3b1c16643ff165ad2e6
ricardoalexandrepereira <ricardo_certe@hotmail.com>
2022-04-26 10:49:13 +0100

    deploy fixed

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectToDb = () => {
   mongoose
     .connect(
-     process.env.DB_URI,
+      process.env.DB_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
